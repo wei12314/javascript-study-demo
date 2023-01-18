d08c1596a703e611605022894689c524f4bc1087
zeng haowei <1642509490@qq.com>
2023-01-18 23:19:48 +0800

    bank first step template and routing

---
+++ ms-demo/bank-project/app.js
@@ -0,0 +1,32 @@
+const routes = {
+  "/login": { templateId: "login" },
+  "/dashboard": { templateId: "dashboard" },
+};
+
+function updateRoute() {
+  const path = window.location.pathname;
+  const route = routes[path];
+  if (!route) {
+    return navigate("login");
+  }
+
+  const template = document.getElementById(route.templateId);
+  const view = template.content.cloneNode(true);
+  const app = document.getElementById("app");
+  app.innerHTML = "";
+  app.appendChild(view);
+}
+
+function navigate(path) {
+  window.history.pushState({}, path, path);
+  updateRoute();
+}
+
+function onLinkClick(event) {
+  event.preventDefault();
+  navigate(event.target.href);
+}
+
+window.onpopstate = () => updateRoute();
+updateRoute();
+//updateRoute("login");
