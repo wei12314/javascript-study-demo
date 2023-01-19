a9e3e0a039a73a75da3b7af73f15bd0fa50aab9b
zeng haowei <1642509490@qq.com>
2023-01-19 22:29:19 +0800

    add css for bank-app project

--- ms-demo/bank-project/app.js
+++ ms-demo/bank-project/app.js
@@ -1,8 +1,12 @@
+//npx lite-server to active server to run single-app
+
+//routes
 const routes = {
   "/login": { templateId: "login" },
   "/dashboard": { templateId: "dashboard" },
 };
 
+//according to routes to update content
 function updateRoute() {
   const path = window.location.pathname;
   const route = routes[path];
@@ -17,6 +21,7 @@ function updateRoute() {
   app.appendChild(view);
 }
 
+//navigate path
 function navigate(path) {
   window.history.pushState({}, path, path);
   updateRoute();
@@ -25,6 +30,38 @@ function navigate(path) {
 function onLinkClick(event) {
   event.preventDefault();
   navigate(event.target.href);
+}
+
+/* Retrieve the form data
+ Convert and encode the form data to a suitable format
+ Create the HTTP request and send it to the server */
+//single page app principle let web page as a app
+async function register() {
+  const registerForm = document.getElementById("registerForm");
+  const formData = new FormData(registerForm);
+  const data = Object.fromEntries(formData);
+  const jsonData = JSON.stringify(data);
+  const result = await createAccount(jsonData);
+
+  if (result.error) {
+    registerForm.after(`<p>${result.error}</p>`);
+    return console.log("An error occurred:", result.error);
+  }
+
+  console.log("Account created!", result);
+}
+
+async function createAccount(account) {
+  try {
+    const response = await fetch("//localhost:5000/api/accounts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: account,
+    });
+    return await response.json();
+  } catch (error) {
+    return { erro: error.messaeg || "Unknown error" };
+  }
 }
 
 window.onpopstate = () => updateRoute();
